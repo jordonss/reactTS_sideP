1211dc5edcd3f754bec3b5915c83e752068e1a52
jordonss <nemezg@gmail.com>
2023-12-06 16:11:51 +0200

    session_components

---
+++ src/components/Sessions/UpcommingSession.tsx
@@ -0,0 +1,37 @@
+import Button from '../UI/Button';
+
+type UpcommingSessionProps = {
+  session: {
+    id: string;
+    title: string;
+    summary: string;
+    date: string;
+  };
+  onCancel: () => void;
+};
+
+export default function UpcommingSession({
+  session,
+  onCancel,
+}: UpcommingSessionProps) {
+  return (
+    <article className='upcomming-session'>
+      <div>
+        <h3>{session.title}</h3>
+        <p>{session.summary}</p>
+        <time dateTime={new Date(session.date).toISOString()}>
+          {new Date(session.date).toLocaleString('en-US', {
+            day: 'numeric',
+            month: 'short',
+            year: 'numeric',
+          })}
+        </time>
+      </div>
+      <p className='actions'>
+        <Button textOnly onClick={onCancel}>
+          Cancel
+        </Button>
+      </p>
+    </article>
+  );
+}
